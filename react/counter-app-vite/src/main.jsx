26d9f47b06177e85ddadad79f896181feeb316e3
erikarubi25 <erikarubi.2579@gmail.com>
2023-05-09 10:08:46 -0600

    practice react eliminando archivos para hacer de nuevo

--- react/counter-app-vite/src/main.jsx
+++ react/counter-app-vite/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//Siempre debe de contar con estas partes:
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Siempre debe de contener un componente q no siempre estara en el index
+function App(){
+    // document.createElement.... Seria como normalmente se escribe sin react
+
+
+    return (<h1>Hola mundo!!!</h1>); //Muchas personas acostumbran a usar parentesis para delimitar por donde empieza y donde termina
+}
+
+//Para renderizar se usa
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
